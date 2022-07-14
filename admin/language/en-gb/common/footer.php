d7b5598cbbe6b4428eda18c165b9a6704cf2fcae
MohAbdelwahab11 <109307614+MohAbdelwahab11@users.noreply.github.com>
2022-07-14 22:10:27 +0200

    Initial commit

---
+++ admin/language/en-gb/common/footer.php
@@ -0,0 +1,4 @@
+<?php
+// Text
+$_['text_footer']  = 'BuyBye &copy; 2021 -' . date('Y') . ' All Rights Reserved.';
+$_['text_version'] = 'Developed By: M, S, A, M - Powered By: Nile University ';
---
+++ config.php
@@ -0,0 +1,30 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'https://bb.7dod.net/');
+
+// HTTPS
+define('HTTPS_SERVER', 'https://bb.7dod.net/');
+
+// DIR
+define('DIR_APPLICATION', '/home1/elol/bb.7dod.net/catalog/');
+define('DIR_SYSTEM', '/home1/elol/bb.7dod.net/system/');
+define('DIR_IMAGE', '/home1/elol/bb.7dod.net/image/');
+define('DIR_STORAGE', '/home1/elol/ocartdata/storage/');
+define('DIR_LANGUAGE', DIR_APPLICATION . 'language/');
+define('DIR_TEMPLATE', DIR_APPLICATION . 'view/theme/');
+define('DIR_CONFIG', DIR_SYSTEM . 'config/');
+define('DIR_CACHE', DIR_STORAGE . 'cache/');
+define('DIR_DOWNLOAD', DIR_STORAGE . 'download/');
+define('DIR_LOGS', DIR_STORAGE . 'logs/');
+define('DIR_MODIFICATION', DIR_STORAGE . 'modification/');
+define('DIR_SESSION', DIR_STORAGE . 'session/');
+define('DIR_UPLOAD', DIR_STORAGE . 'upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'elol_mams');
+define('DB_PASSWORD', 'K8[]ApS113');
+define('DB_DATABASE', 'elol_mams');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'mams_');
